3e1f7be72ad3c319399eaa08a045f18f19aa7b04
Dave Latham <dave.latham@gmail.com>
2018-06-25 11:51:53 -0700

    Updated to get priority from priorityid

--- app/controllers/bug_controller.rb
+++ app/controllers/bug_controller.rb
@@ -19,15 +19,15 @@ class BugController < ApplicationController
   	@title = 'Case ' + params['casenumber'] + ': ' + params['title']
   	@link = ENV['MANUSCRIPT_URL'] + '/f/cases/' + params['casenumber']
   	
-  	if params['category'] == '1'
+  	if params['priorityid'] == '1'
   		@message = [{fallback: params['title'], color: '#BB4430', pretext: 'A new bug was just created...', title: @title, author_name: params['personeditingname'], title_link: @link, text: params['eventtext'].truncate(50), fields: [{title: 'Priority', value: 'High', short: 'false'}]}]
   		client.chat_postMessage(channel: '#p1-bugs', attachments: @message, as_user: true)
   		render status: 200, body: 'Successful p1 chat'
-  	elsif params['category'] == '2'
+  	elsif params['priorityid'] == '2'
   		@message = [{fallback: params['title'], color: '#ED7D3A', pretext: 'A new bug was just created...', title: @title, author_name: params['personeditingname'], title_link: @link, text: params['eventtext'].truncate(50), fields: [{title: 'Priority', value: 'Medium', short: 'false'}]}]
   		client.chat_postMessage(channel: '#p2-bugs', attachments: @message, as_user: true)
   		render status: 200, body: 'Successful p2 chat'
-  	elsif params['category'].to_i >= 3 
+  	elsif params['priorityid'].to_i >= 3 
   		#@message = [{fallback: params['title'], color: '#36a64f', pretext: 'A new bug was just created...', title: @title, author_name: params['personeditingname'], title_link: @link, text: params['eventtext'].truncate(50), fields: [{title: 'Priority', value: 'Low', short: 'false'}]}]
   		#client.chat_postMessage(channel: '#p3-bugs', attachments: @message, as_user: true)
   		render status: 200, body: 'Bug status change recognized but not sent to chat'
