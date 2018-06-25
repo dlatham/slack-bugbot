682e50e18c9dcf3060b0cb732bfdc89663520e8e
Dave Latham <dave.latham@gmail.com>
2018-06-25 12:04:17 -0700

    added case closed route

--- app/controllers/bug_controller.rb
+++ app/controllers/bug_controller.rb
@@ -4,16 +4,6 @@ class BugController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :check_token
 
-  def new
-  end
-
-  def comment
-  end
-
-  def status
-  
-  end
-
   def status_update
   	client = Slack::Web::Client.new
   	@title = 'Case ' + params['casenumber'] + ': ' + params['title']
@@ -34,6 +24,29 @@ class BugController < ApplicationController
   	else
   		render status: 400, body: 'Incorrectly formatted request'
   	end
+  end
+
+  def status_closed
+  	client = Slack::Web::Client.new
+  	@title = 'Case ' + params['casenumber'] + ': ' + params['title']
+  	@link = ENV['MANUSCRIPT_URL'] + '/f/cases/' + params['casenumber']
+
+  	if params['priorityid'] == '1'
+  		@message = [{fallback: params['title'], color: '#36a64f', pretext: 'A bug was just resolved...', title: @title, author_name: params['personeditingname'], title_link: @link, text: params['eventtext'].truncate(50), fields: [{title: 'Status', value: 'Resolved', short: 'false'}]}]
+  		client.chat_postMessage(channel: '#p1-bugs', attachments: @message, as_user: true)
+  		render status: 200, body: 'Successful p1 chat'
+  	elsif params['priorityid'] == '2'
+  		@message = [{fallback: params['title'], color: '#36a64f', pretext: 'A bug was just resolved...', title: @title, author_name: params['personeditingname'], title_link: @link, text: params['eventtext'].truncate(50), fields: [{title: 'Status', value: 'Resolved', short: 'false'}]}]
+  		client.chat_postMessage(channel: '#p2-bugs', attachments: @message, as_user: true)
+  		render status: 200, body: 'Successful p2 chat'
+  	elsif params['priorityid'].to_i >= 3 
+  		#@message = [{fallback: params['title'], color: '#36a64f', pretext: 'A new bug was just created...', title: @title, author_name: params['personeditingname'], title_link: @link, text: params['eventtext'].truncate(50), fields: [{title: 'Priority', value: 'Low', short: 'false'}]}]
+  		#client.chat_postMessage(channel: '#p3-bugs', attachments: @message, as_user: true)
+  		render status: 200, body: 'Bug status change recognized but not sent to chat'
+  	else
+  		render status: 400, body: 'Incorrectly formatted request'
+  	end
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'bug/comment'
   post 'bug/status'
   post 'bug/status_update', to: 'bug#status_update'
+  post 'bug/status_closed', to: 'bug#status_closed'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
